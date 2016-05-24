2f17e7c5bad800b82711eedaeaac21c539381f87
Tim Russo <timrusso@Tims-iMac.local>
2016-05-24 18:00:33 +0300

    fix device name

--- src/test/java/applogic1/ApplicationManager1.java
+++ src/test/java/applogic1/ApplicationManager1.java
@@ -36,7 +36,7 @@ public class ApplicationManager1 implements ApplicationManager {
         capabilities.setCapability("automationName", "Appium");
 
         capabilities.setCapability("platformVersion", "4.4");
-        capabilities.setCapability("deviceName", "Nexus_5X_API_23");
+        capabilities.setCapability("deviceName", "emulator-5748");
 
         File file = new File("/Users/timrusso/TIM/test/android/apps/app-advisor-debug.apk");
         capabilities.setCapability("app", file.getAbsolutePath());
